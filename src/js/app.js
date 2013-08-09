141c703a534d98c90cd6d5cb09221dd8ea4f969e
rhannequin <remy.hannequin@gmail.com>
2013-08-10 00:34:22 +0200

    Ajax request to /api/sun + display range attribute

--- src/js/app.js
+++ src/js/app.js
@@ -5,8 +5,13 @@
     // Create the XHR object.
     var _createCORSRequest = function (method, url) {
       var xhr = new XMLHttpRequest();
-      if ("withCredentials" in xhr) {
+      if("withCredentials" in xhr) {
+        // XHR for Chrome/Firefox/Opera/Safari.
         xhr.open(method, url, true);
+      } else if (typeof XDomainRequest !== 'undefined') {
+        // XDomainRequest for IE.
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
       } else {
         // CORS not supported.
         xhr = null;
@@ -19,9 +24,11 @@
       var method  = opts.method  || 'GET',
           data    = opts.data    || {},
           success = opts.success || function(){},
-          error   = opts.error || function(){};
+          error   = opts.error   || function (response) {
+            console.error('XHR failed:', response);
+          };
       var xhr = _createCORSRequest(method, url);
-      if (!xhr) {
+      if(!xhr) {
         console.error('CORS not supported');
         return;
       }
@@ -32,7 +39,7 @@
         if(xhr.readyState === completed){
           if(xhr.status === 200){
             success(xhr.responseText, xhr);
-          }else{
+          } else {
             error(xhr.responseText, xhr);
           }
         }
@@ -45,25 +52,42 @@
       xhr.send(data);
     };
 
-    var obj = {};
-
-    obj.ajax = function (url, opts) {
+    this.ajax = function (url, opts) {
       _makeCorsRequest(url, opts);
     };
 
-    return obj;
+    return this;
 
   }).call(this);
 
-  document.querySelector('#btn-sun').addEventListener('click', function (e) {
-    e.preventDefault();
-    document.querySelector('#sun').className = 'current';
-    document.querySelector('[data-position="current"]').className = 'left';
-  });
-  document.querySelector('#btn-sun-back').addEventListener ('click', function (e) {
-    e.preventDefault();
-    document.querySelector('#sun').className = 'right';
-    document.querySelector('[data-position="current"]').className = 'current';
-  });
+  var sunDom = document.querySelector('#sun');
+
+  if(sunDom) {
+
+    document.querySelector('#btn-sun').addEventListener('click', function (e) {
+      e.preventDefault();
+      var contentDom = sunDom.querySelector('.content');
+      document.querySelector('#sun').className = 'current';
+      document.querySelector('[data-position="current"]').className = 'left';
+      if(contentDom && !contentDom.innerHTML.length) {
+        contentDom.innerHTML = '<p><img src="img/loader.gif" alt="Loader" /> Loading...</p>';
+        Util.ajax('http://localhost:5000/api/sun?pretty=false', {
+          success: function (res) { sunSuccessCb(res, contentDom); }
+        });
+      }
+    });
+
+    document.querySelector('#btn-sun-back').addEventListener ('click', function (e) {
+      e.preventDefault();
+      document.querySelector('#sun').className = 'right';
+      document.querySelector('[data-position="current"]').className = 'current';
+    });
+
+  }
+
+  function sunSuccessCb (response, contentDom) {
+    var data = JSON.parse(response).data;
+    contentDom.innerHTML = '<p>Sun range: ' + data.range + ' AU</p>';
+  }
 
 }).call(this);
